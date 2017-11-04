fe63158e5c75e8fdfd07579b85cdda5f070424f7
Michal Michalczuk <michalczukm@gmail.com>
2017-11-04 12:52:42 +0100

    add gulp with usage

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+    return gulp.src('src/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['scripts']);
